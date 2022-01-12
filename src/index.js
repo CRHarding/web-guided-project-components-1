45c9c3c071efecc36eecdd4a9729196c09d551aa
Casey Harding <casey.r.harding@gmail.com>
2022-01-12 12:54:24 -0500

    Initial setup in place.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ const imageData = [
   { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg' },
 ]
 // const makeImage = (image) => {}
-function makeImage(image) {
+function makeImage({ imageURL }) {
+  // const imageURL = image.imageURL
   const dogImg = document.createElement('img');
-  dogImg.src = image.imageURL;
+  dogImg.src = imageURL;
   dogImg.style.height = '10em';
   return dogImg;
 }
@@ -22,32 +23,40 @@ imageData.forEach(imgObj => {
 //  On the one hand, the default export from data/panelData.js
 //  On the other hand, the default export from data/constants.js
 //  Destructure `open` and `close` from the constants
+import panelData from './data/panelData';
+import constants from './data/constants';
+import linkData from './data/linkData';
 
+const { open, close } = constants;
+/**
+ * const open = constants.open;
+ * const close = constants.close;
+ */
 
 // TASK 2- Verify our imports using log statements
-console.log() // log the panelData
-console.log() // log the open arrow
-console.log() // log the close arrow
+// console.log(panelData); // log the panelData
+// console.log(open); // log the open arrow
+// console.log(close); // log the close arrow
 
 
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
-const accordion = null
+const accordion = document.querySelector('.accordion');
 
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
-function makePanel(/* what data does the panel need? */) {
+function makePanel(panelData) {
 
 
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
+  const panel = document.createElement('div');
+  const panelBar = document.createElement('div');
+  const panelContent = document.createElement('div');
+  const panelTitle = document.createElement('h3');
+  const panelButtons = document.createElement('div');
+  const openButton = document.createElement('button');
+  const closeButton = document.createElement('button');
 
 
   // TASK 6- Setup the structure of our elements
@@ -63,10 +72,29 @@ function makePanel(/* what data does the panel need? */) {
       <div></div>           // panelContent
     </div>
   */
-
+  panel.appendChild(panelBar);
+  panel.appendChild(panelContent); // <div><div></div><div></div></div>
+  panelBar.appendChild(panelTitle);
+  panelBar.appendChild(panelButtons);
+  panelButtons.appendChild(openButton);
+  panelButtons.appendChild(closeButton);
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
+  // <!-- Remove this -->
+  // <div class="panel">
+  //   <div class="panel-bar">
+  //     <h3>Title of Panel</h3>
+  //     <div class="panel-buttons">
+  //       <button class="panel-btn-open">&#9660</button>
+  //       <button class="panel-btn-close hide-btn">Close</button>
+  //     </div>
+  //   </div>
+  //   <div class="panel-content toggle-on">
+  //     Content of panel
+  //   </div>
+  // </div>
+  // <!-- Remove this -->
 
 
   // TASK 8- Set text content using arguments as raw material
@@ -80,9 +108,13 @@ function makePanel(/* what data does the panel need? */) {
 
 
   // don't forget to return the panel!
-  return null
+  return panel;
 }
 
+// const testData = { title: 'foo', content: 'bar' };
+// const testPanel = makePanel(testData);
+const testPanel = makePanel({ title: 'foo', content: 'bar' });
+accordion.appendChild(testPanel);
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
